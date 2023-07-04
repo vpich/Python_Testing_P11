3a5fdf141c1fa7f005046778ce3c2a90c5389b2f
Virginie PICH <red_iv@hotmail.fr>
2023-07-04 11:12:53 +0200

    Test: ajout de test pour vérifier qu'un utilisateur ne peut acheter de places uniquement s'il a un solde de points suffisant

--- tests/tests_fonctionnels/test_urls.py
+++ tests/tests_fonctionnels/test_urls.py
@@ -56,3 +56,19 @@ class UrlsTest(TestCase):
         assert response.status_code == 200
         self.assertTemplateUsed("welcome.html")
         self.assertContext("club", FAKE_CLUBS[0])
+
+    # BUG n2
+    def test_purchase_places_unavalaible_points_fail(self):
+        response = self.client.post("/purchasePlaces", data={
+            "competition": FAKE_COMPETITIONS[0]["name"],
+            "club": FAKE_CLUBS[0]["name"],
+            "places": 4})
+        assert response.status_code == 400
+        assert b"not allowed to use unavalaible points." in response.data
+
+    def test_purchase_places_success(self):
+        response = self.client.post("/purchasePlaces", data={
+            "competition": FAKE_COMPETITIONS[0]["name"],
+            "club": FAKE_CLUBS[0]["name"],
+            "places": 1})
+        assert response.status_code == 200
